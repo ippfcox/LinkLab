3370c97250379329484104cb2c4c83e9860e34f7
fcoxx <ippfcox@outlook.com>
2017-02-23 11:15:37 +0800

    first commit

---
+++ LinkLab/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace LinkLab {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.panel_output = new System.Windows.Forms.Panel();
+            this.panel_cmd = new System.Windows.Forms.Panel();
+            this.richTextBox_cmd = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.调试ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cMDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel_cmd.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_output
+            // 
+            this.panel_output.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_output.Location = new System.Drawing.Point(13, 27);
+            this.panel_output.Name = "panel_output";
+            this.panel_output.Size = new System.Drawing.Size(259, 86);
+            this.panel_output.TabIndex = 0;
+            // 
+            // panel_cmd
+            // 
+            this.panel_cmd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_cmd.Controls.Add(this.richTextBox_cmd);
+            this.panel_cmd.Location = new System.Drawing.Point(13, 119);
+            this.panel_cmd.Name = "panel_cmd";
+            this.panel_cmd.Size = new System.Drawing.Size(259, 131);
+            this.panel_cmd.TabIndex = 1;
+            // 
+            // richTextBox_cmd
+            // 
+            this.richTextBox_cmd.Location = new System.Drawing.Point(-1, 0);
+            this.richTextBox_cmd.Name = "richTextBox_cmd";
+            this.richTextBox_cmd.Size = new System.Drawing.Size(260, 131);
+            this.richTextBox_cmd.TabIndex = 0;
+            this.richTextBox_cmd.Text = "";
+            this.richTextBox_cmd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox_cmd_KeyDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.调试ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 25);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 调试ToolStripMenuItem
+            // 
+            this.调试ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cMDToolStripMenuItem});
+            this.调试ToolStripMenuItem.Name = "调试ToolStripMenuItem";
+            this.调试ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.调试ToolStripMenuItem.Text = "调试";
+            // 
+            // cMDToolStripMenuItem
+            // 
+            this.cMDToolStripMenuItem.Name = "cMDToolStripMenuItem";
+            this.cMDToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cMDToolStripMenuItem.Text = "CMDHistory";
+            this.cMDToolStripMenuItem.Click += new System.EventHandler(this.cMDToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.panel_cmd);
+            this.Controls.Add(this.panel_output);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel_cmd.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_output;
+        private System.Windows.Forms.Panel panel_cmd;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox_cmd;
+        private System.Windows.Forms.ToolStripMenuItem 调试ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cMDToolStripMenuItem;
+    }
+}
+
---
+++ LinkLab/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LinkLab {
+    public partial class Form1 : Form {
+        private string CMDHead = "LinkLab>";
+        private List<CMD> CMDHistory = new List<CMD>();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            richTextBox_cmd.Font = new System.Drawing.Font("consolas", 10f);
+            InitializeCMD();//这个放在这里的原因是修改字体时将颜色给改了，也就是说，构造函数先于这个函数执行（这不废话么）
+            //debug
+            panel_output.BorderStyle = BorderStyle.FixedSingle;
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            panel_cmd.BorderStyle = BorderStyle.FixedSingle;
+        }
+        //初始化新的命令
+        private void InitializeCMD() {
+            richTextBox_cmd.SelectionColor = Color.Red;
+            richTextBox_cmd.AppendText(CMDHead);
+            richTextBox_cmd.SelectionColor = Color.Black;
+        }
+        //检测回车，进入下个命令
+        private void richTextBox_cmd_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                richTextBox_cmd.AppendText("\r\n");
+                e.Handled = true;
+                CMD newCMD = new CMD(richTextBox_cmd.Text);
+                CMDHistory.Add(newCMD);
+                InitializeCMD();
+            }
+        }
+        //输出点调试信息
+        private void cMDToolStripMenuItem_Click(object sender, EventArgs e) {
+            string allCMDs = "";
+            foreach (CMD one in CMDHistory) {
+                allCMDs += one.GetCMD() + "\r\n";
+            }
+            MessageBox.Show(allCMDs);
+        }
+    }
+    //命令内容相关的类
+    public class CMD {
+        private string currentCMD = "";
+        //构造函数，从窗口中获得命令本身
+        public CMD(string allCMD) {
+            string[] CMDLines = allCMD.Split(new string[] { "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            string currentLine = CMDLines[CMDLines.Count() - 1];
+            currentCMD = currentLine.Split(new char[] { '>' }, 2)[1];
+        }
+        //返回具体命令内容
+        public string GetCMD() {
+            return currentCMD;
+        }
+    }
+}
